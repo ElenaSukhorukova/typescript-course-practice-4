71e1a4f8158d6a747df1f93d30d70e51e8f1697a
Elena Sukhorukova <elena@reactivepay.com>
2025-03-10 09:07:46 +0400

    Add some logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,61 @@
-import { useState } from 'react';
+import { type ReactNode, useEffect, useState } from 'react';
 import { get } from "./util/http";
 
-function App() {
-  useState();
-  get('https://jsonplaceholder.typicode.com/posts');
+import BlogPosts, { type BlogPost } from './components/BlogPosts';
+import ErrorMessage from './components/ErrorMessage';
 
-  return <h1>Data Fetching!</h1>;
+import fetchingImg from './assets/data-fetching.png';
+
+type RawDataBlogPost = {
+  id: number;
+  title: string;
+  body: string;
+  userId: number;
+}
+
+function App() {
+  const [fetchedPosts, setFetchedPOsts] = useState<BlogPost[] | undefined>();
+  const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState<string | undefined>();
+
+  useEffect(() => {
+    async function fetchPosts() {
+      setIsFetching(true);
+
+     try {
+      const data = (await get(
+        'https://jsonplaceholder.typicode.com/postsddddd'
+      )) as RawDataBlogPost[];
+
+      const blogPosts: BlogPost[] = data.map(rawPost => {
+        return {
+          id: rawPost.id,
+          title: rawPost.title,
+          text: rawPost.body
+        }
+      })
+
+      setFetchedPOsts(blogPosts);
+     } catch(error) {
+      if (error instanceof Error) {
+        setError(error.message);
+      }
+     }
+
+      setIsFetching(false);
+    }
+
+    fetchPosts();
+  }, []);
+
+  return (
+    <main>
+      <img src={fetchingImg} alt="An abstract image depicting a data fetching process." />
+      {isFetching && <p id="loading-fallback">Fetching posts...</p>}
+      {fetchedPosts && <BlogPosts posts={fetchedPosts} />}
+      {error && <ErrorMessage text={error} />}
+    </main>
+  )
 }
 
 export default App;
